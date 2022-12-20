37786edf716d144a220cdc83182cbc97b5ea6848
Denys Diachenko <denys.diachenko@Denys.Diachenko>
2022-12-20 14:07:02 +0200

    Finished lab 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { delay } from "rxjs/operators";
+
+export interface Todo {
+  id?: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'lab-6';
+export class AppComponent implements OnInit {
+  namePost = '';
+  posts: Todo[] = [];
+  flagLoad = false;
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.loadPost();
+  }
+  addPost() {
+    if (!this.namePost.trim()) {
+      return
+    }
+    const post: Todo = { title: this.namePost, completed: false };
+    this.http.post('https://jsonplaceholder.typicode.com/todos/', post)
+      .subscribe(res => {
+        console.log(res);
+        this.posts.unshift(post)
+      });
+  }
+  loadPost() {
+    this.flagLoad = true;
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .pipe(delay(1500))
+      .subscribe(response => {
+        console.log(response);
+        this.posts = response.sort((a, b) => a.title.localeCompare(b.title));
+        this.flagLoad = false;
+      });
+  }
+  removePost(id: number | undefined) {
+    if (!id) {
+      return;
+    }
+    this.http.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .subscribe(() => {
+        console.log(id);
+        this.posts = this.posts.filter(item => item.id != id);
+        console.log(this.posts)
+      });
+  }
+  completedPost(id: number | undefined) {
+    if (!id) {
+      return;
+    }
+    this.http.put(`https://jsonplaceholder.typicode.com/todos/${id}`,{completed:true})
+      .subscribe((res: any) => {
+        this.posts.find(item=> item.id===res.id)!.completed=true})
+  }
 }
